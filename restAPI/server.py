219ca10ce1323b22a4c887eed86ceb7191aac208
Rex <dazlr@protonmail.ch>
2019-10-23 00:37:58 +0530

    restAPI

--- restAPI/server.py
+++ restAPI/server.py
@@ -11,7 +11,6 @@ api = Api(app)
 class Employees(Resource):
     def get(self):
         conn = db_connect.connect()  # connect to database
-        print(conn)
         query = conn.execute("select * from employees")  # This line performs query and returns json result
         return {'employees': [i[0] for i in query.cursor.fetchall()]}  # Fetches first column that is Employee ID
 
